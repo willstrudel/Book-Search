7c3dd16acbbda458921bb4cc6fcbdfe3db90f40d
Will <willstruder@gmail.com>
2021-08-25 00:05:39 -0400

    Updating typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,7 +34,7 @@ type Query {
 type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(bookData; bookInput!): User
+    saveBook(bookData: bookInput!): User
     removeBook(bookId: ID!): User
 }
 
