a4b6130d9ed4f72766d049cf8f49303c0f151fbb
Will <willstruder@gmail.com>
2021-08-25 00:53:23 -0400

    Updated resolvers file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
-const { Book } = require('../models');
+const {
+    Book
+} = require('../models');
 
 const resolvers = {
     Query: {
@@ -9,9 +11,111 @@ const resolvers = {
             });
         },
 
-        book: async (parent, {bookId}) => {
-            return await Class.find({}).populate('author');
+        Mutation: {
+            login: async (parent, {
+                username,
+                email,
+                password
+            }) => {
+                const user = await User.findOne({
+                    $or: [{
+                        username: username
+                    }, {
+                        email: email
+                    }],
+                });
+
+                if (!user) {
+                    throw new AuthenticationError(
+                        "No user found with this username or email address"
+                    );
+                }
+
+                const correctPw = await user.isCorrectPassword(password);
+
+                if (!correctPw) {
+                    throw new AuthenticationError("Password incorrect");
+                }
+
+                const token = signToken(user);
+
+                return {
+                    token,
+                    user
+                };
+            },
+            addUser: async (parent, {
+                username,
+                email,
+                password
+            }) => {
+                const user = await User.create({
+                    username,
+                    email,
+                    password
+                });
+                const token = signToken(user);
+                return {
+                    token,
+                    user
+                };
+            },
+            saveBook: async (
+                parent, {
+                    authors,
+                    description,
+                    bookId,
+                    image,
+                    link,
+                    title
+                },
+                context
+            ) => {
+                if (context.user) {
+                    const updatedUser = await User.findOneAndUpdate({
+                        _id: context.user._id
+                    }, {
+                        $addToSet: {
+                            savedBooks: {
+                                authors,
+                                description,
+                                bookId,
+                                image,
+                                link,
+                                title
+                            },
+                        },
+                    }, {
+                        new: true,
+                        runValidators: true
+                    });
+
+                    return updatedUser;
+                }
+                throw new AuthenticationError("Must be logged in to view this page.");
+            },
+            removeBook: async (parent, {
+                bookId
+            }, context) => {
+                if (context.user) {
+                    const updatedUser = await User.findOneAndUpdate({
+                        _id: context.user._id
+                    }, {
+                        $pull: {
+                            savedBooks: {
+                                bookId: bookId
+                            }
+                        }
+                    }, {
+                        new: true
+                    });
+
+                    return updatedUser;
+                }
+                throw new AuthenticationError("Must be logged in to view this page.");
+            },
         },
+
     },
 };
 
--- server/server.js
+++ server/server.js
@@ -3,16 +3,18 @@ const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const {authMiddleware } = require('./utils/auth');
-const { resolvers, typeDefs } = require('./schemas')
+const { typeDefs, resolvers } = require('./schemas')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
-  resolvers,
   typeDefs,
+  resolvers,
   context: authMiddleware
 });
+
+server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
